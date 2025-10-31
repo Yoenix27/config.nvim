e83b2e4d94f49bd1c25f37a67536d0b7ee71a4aa
Yoenix27 <wofwall@gmail.com>
2025-11-01 01:19:59 +0200

    adding oil and modify keybinds

--- templates/cpp_template.cpp
+++ templates/cpp_template.cpp
@@ -1,15 +1,19 @@
+//  __     __
+//  \ \   / /
+//   \ \_/ /__  _   _ _ __ (_)_ __  ___
+//    \   / _ \| | | | '_ \| | '_ \/ __|
+//     | | (_) | |_| | | | | | | | \__ \
+//     |_|\___/ \__,_|_| |_|_|_| |_|___/
+//
 // Problem:
 // Author: younis
-// Created: $(DATE)
-
-
-
+// Created: Fri 31 Oct 2025 22:23
 
 #include <iostream>
-
 using namespace std;
+
 int main() {
-cout<<"hello, world!";
-  return 0;
+    cout << "hello, world!";
+    return 0;
 }
 
