107ddc55169899eb3e88363b0ad66c5dbb316813
Yoenix27 <wofwall@gmail.com>
2025-10-31 13:52:12 +0200

    lazy is here

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,7 +8,7 @@ vim.opt.cursorline = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 
-vim.opt.shiftwidth = 2
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.wrap = false
