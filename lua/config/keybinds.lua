20e2dfb63c6f4b268053f47adf2748261d000ad8
Yoenix27 <wofwall@gmail.com>
2025-11-08 11:44:36 +0200

    add competitest

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,3 +1,9 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "."
 vim.keymap.set("n", "<leader>e", vim.cmd.Oil)
+--Competitive Programming
+vim.keymap.set("n", "<localleader>t", "<cmd>CompetiTest run<CR>")
+vim.keymap.set("n", "<localleader>rc", "<cmd>CompetiTest receive contest<CR>")
+vim.keymap.set("n", "<localleader>rp", "<cmd>CompetiTest receive problem<CR>")
+vim.keymap.set("n", "<localleader>at", "<cmd>CompetiTest add_testcase<CR>")
+vim.keymap.set("n", "<localleader>et", "<cmd>CompetiTest edit_testcase<CR>")
