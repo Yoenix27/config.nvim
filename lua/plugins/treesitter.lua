17b9144039e81699bc71b34e8e22d464e149a40a
yosunnyvim <uniscore@proton.me>
2026-04-07 05:27:28 +0200

    rm: default template

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,7 +6,7 @@ return {
     require("nvim-treesitter.configs").setup({
       ensure_installed = {
         "vim", "vimdoc", "lua", "make", "bash", "markdown", "markdown_inline",  "yaml", 
-        "c", "cpp", "asm",  "html", "css", "javascript", "go","c_sharp" ,"python", "java", "sql", "gomod",
+        "c", "cpp", "asm",  "html", "css", "javascript", "go","c_sharp" ,"python","java", "sql", "gomod",
       },
       highlight = { enable = true },
       indent = { enable = true },
